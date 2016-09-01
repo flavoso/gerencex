5aed4b6e08f9ad3af1ff8c539a094aeeb6674554
Flavio Cardoso <flavoso@gmail.com>
2016-09-01 10:46:09 -0300

    Começando a customizar o admin para o model Timing

--- gerencex/core/admin.py
+++ gerencex/core/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from gerencex.core.models import UserDetail, Timing, Restday
+from gerencex.core.models import UserDetail, Timing, Restday, HoursBalance
 
 
 # Define an inline admin descriptor for Employee model
@@ -17,13 +17,20 @@ class UserDetailInLine(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (UserDetailInLine, )
 
+
+class TimingAdmin(admin.ModelAdmin):
+    list_display = ('date_time', 'user', 'checkin', 'created_by')
+    list_filter = ('date_time', 'user', 'created_by')
+    date_hierarchy = 'date_time'
+
+
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
 
-
 # Register your models here.
 
-admin.site.register(Timing)
+admin.site.register(Timing, TimingAdmin)
 admin.site.register(Restday)
+admin.site.register(HoursBalance)
