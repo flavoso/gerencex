0729f06d200c502686c0c14eca67e8ff510fa54f
Flavio Cardoso <flavoso@gmail.com>
2016-12-02 14:58:05 -0200

    1) Improved the 'calculations' view; 2) All templates are, now, mobile friedly.

--- gerencex/core/templatetags/user_tags.py
+++ gerencex/core/templatetags/user_tags.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import template
 
 
@@ -11,3 +13,16 @@ def has_group(user, group_name):
     """
     groups = user.groups.all().values_list('name', flat=True)
     return True if group_name in groups else False
+
+
+@register.filter('timedelta')
+def timedelta(delta):
+    """
+    :param delta: a datetime.timedelta value
+    :return: a string in the format H:i:s or -H:i:s
+    """
+    seconds = int(delta.total_seconds())
+
+    if seconds < 0:
+        return '-{}'.format(datetime.timedelta(seconds=abs(seconds)))
+    return str(datetime.timedelta(seconds=seconds))
